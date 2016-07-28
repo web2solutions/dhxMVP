89e9023f54fa63245eadfce74ac135ffb48581d0
web2solutions <eduardo@web2solutions.com.br>
2016-07-28 15:03:55 -0300

    removing git-test

--- Gulpfile.js
+++ Gulpfile.js
@@ -172,7 +172,7 @@ var paths = ["lib", "lib/view", "lib/presenter", "lib/model", "lib/dhx", /*"lib/
             end_date,
             elapsed_time;
         return gulp
-            .src('./git-test/*')
+            .src('./*')
             .pipe(git.add())
             .on('finish', function() {
                 end_date = new Date(),
@@ -192,7 +192,7 @@ var paths = ["lib", "lib/view", "lib/presenter", "lib/model", "lib/dhx", /*"lib/
             elapsed_time,
             total_data_stream = 0;
         return gulp
-            .src('./git-test/*')
+            .src('./*')
             .pipe(git.add())
             .pipe(git.commit('testing git via gulp 12', {emitData:true}))
             .on('data',function(data) {
